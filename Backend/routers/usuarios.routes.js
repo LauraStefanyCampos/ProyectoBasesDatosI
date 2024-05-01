26575122a801a0fa1f6275b880ca303e0e9f2b4f
LauraStefanyCampos <125720227+LauraStefanyCampos@users.noreply.github.com>
2024-04-30 22:03:29 -0600

    Delete Backend directory

--- Backend/routers/usuarios.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const express = require('express');
-const usuarioController = require('../controllers/usuarioController');
-const router = express.Router();
-router.get('/usuarios', usuarioController.obtenerTodosLosUsuarios);
-// Definir otras rutas y conectar a los controladores correspondientes...
-module.exports = router;
--- Backend/routers/usuarios.routes.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const usuarioController = require('../controllers/usuarioController');
-
-const router = express.Router();
-
-router.get('/usuarios', usuarioController.obtenerTodosLosUsuarios);
-// Definir otras rutas y conectar a los controladores correspondientes...
-
-module.exports = router;
